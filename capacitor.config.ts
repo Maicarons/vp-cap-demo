5cf0cf2a3124183486e9d87455a3e9c3fd3e407a
Maicarons <136282328+Maicarons@users.noreply.github.com>
2025-11-14 06:41:15 +0000

    Revert "```"
    This reverts commit 93b88dfd06d1f143c924ef5e7d6a5420f4316c0d.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'cc.maicarons.vpcapdemo',
+  appId: 'com.example.vpcapdemo',
   appName: 'VPCapDemo',
   webDir: 'docs/.vitepress/dist',
 }
